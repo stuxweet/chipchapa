07bb02bdccf340c74e297bc4b3af205c26043a48
stuxweet <stuxweet@gmail.com>
2025-05-30 01:12:14 -0300

    Update chip8.h

--- chip8.h
+++ chip8.h
@@ -6,21 +6,21 @@ class Chip8
 {
 private:
 	// 8 bit
-	uint8_t memory[4096]; // memory (4KB)
-	uint8_t V[16];		  // registers (16 8 bit registers)
-	uint8_t sp;			  // stack pointer
+	uint8_t memory[4096]; 		// memory (4KB)
+	uint8_t V[16];		  	// registers (16 8 bit registers)
+	uint8_t sp;			// stack pointer
 	uint8_t delay_timer;
 	uint8_t sound_timer;
 
 	// 16 bit
 	uint16_t I;			// index register
-	uint16_t pc;		// program counter
-	uint16_t stack[16]; // stack
-	uint16_t opcode;	// current opcode
+	uint16_t pc;			// program counter
+	uint16_t stack[16]; 		// stack
+	uint16_t opcode;		// current opcode
 
 public:
-	uint8_t display[64 * 32]; // display (64x32 pixels)
-	uint16_t keypad;		  // key state (16 keys)
+	uint8_t display[64 * 32]; 	// display (64x32 pixels)
+	uint16_t keypad;		// key state (16 keys)
 
 	Chip8();
 	~Chip8();
